d3309e924e0c7f88984bc671261c206cb6b5a671
NahuelGrijalba <nahug07@gmail.com>
2023-08-12 13:06:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AppForm from './form/AppForm'
+
+function App() {
+
+  return (
+    <>
+      <AppForm/>
+    </>
+  )
+}
+
+export default App
---
+++ src/form/AppForm.jsx
@@ -0,0 +1,68 @@
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import validator from "validator"
+
+ function AppForm () {
+
+    // vite - formik - formspree 
+
+    return (
+        <Formik
+        initialValues={{ name: "", email: "", message: "" }}
+        validate={ values => {
+            let errors = {};
+            if(!values.name){
+                errors.name= "Name is required"
+            } else if (!values.email){
+                errors.email= "Email is required"
+            } else if (!validator.isEmail(values.email)){
+                errors.email= "The email is not valid"
+            }
+  
+            return errors;
+        }}
+        onSubmit={
+            (values, { setSubmitting }) => {
+                let url = 'https://formspree.io/f/mbjvgayn';
+                let formData = new FormData();
+                formData.append("name", values.name);
+                formData.append("email", values.email);
+                formData.append("message", values.message);
+
+                fetch(url, {
+                    method: "POST",
+                    body: formData,
+                    headers: {
+                        'Accept': 'application/json'
+                    }
+                }).then(res => {
+                    setSubmitting(false);
+                })
+            }
+        }
+        >
+            {
+                ({isSubmiting}) => (
+                    <Form>
+                        <div>
+                            <label htmlFor="name">Name:</label>
+                            <Field name="name" type="text" />
+                            <ErrorMessage name="name" component="p"/>
+                        </div>
+                        <div>
+                            <label htmlFor="email">Email:</label>
+                            <Field name="email" type="email" />
+                            <ErrorMessage name="email" component="p"/>
+                        </div>
+                        <div>
+                            <label htmlFor="message">Message:</label>
+                            <Field component="textarea" name="message"></Field>
+                        </div>
+                        <button type="submit" disabled={isSubmiting}>{isSubmiting ? "Sending.." : "Send"}</button>
+                    </Form>
+                )
+            }
+        </Formik>
+    )
+}
+
+export default AppForm;
